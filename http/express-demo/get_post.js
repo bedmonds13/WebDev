9773d45a9f5d8b61024b4b7f70f7cddc50b41b1a
bedmonds13 <55567427+bedmonds13@users.noreply.github.com>
2019-10-26 10:02:24 -0700

    one

--- http/express-demo/get_post.js
+++ http/express-demo/get_post.js
@@ -21,22 +21,15 @@ app.get('/api/courses', (req, res) =>{
 
 //Post message for client to POST courses in array
 app.post('/api/courses', (req, res) => {
-    const schema = {//schema to only allow specific input
-        id: Joi.string().min(1).required(),
-        name: Joi.string().min(5).required(),
-        badge: Joi.string().min(8).required()
-    };
-
-    const result = Joi.validate(req.body, schema);
+    const result = validateCourse(req.body)
     if(result.error){
-        console.log(result.error);
         res.status(400).send(result.error);
         return;
    }
 
     const course  = {//creates course Object after validation
-        //id: courses.length + 1,
-        id: req.body.id,
+        id: courses.length + 1,
+        //id: req.body.id,
         name: req.body.name,
         badge: req.body.badge
    };
@@ -44,13 +37,33 @@ app.post('/api/courses', (req, res) => {
    res.send(course);
 });
 
- 
+ app.put('/api/courses/:id', (req, res) =>{
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) res.status(404).send('The course with the given ID does not exist');// 404
+    
+    const result = validateCourse(req.body);
+    if(result.error){
+        res.status(400).send(result.error);
+        return;
+        }
+    course.name = req.body.name;
+    res.send(course);
+});
 
+ function validateCourse(course){
+    const schema = {//schema to only allow specific input
+        name: Joi.string().min(5).required(),
+        badge: Joi.string().min(8).required()
+    }; 
+
+    return Joi.validate(course, schema);
+ }
 app.get('/api/courses/:id', (req, res) =>{
 const course = courses.find(c => c.id === parseInt(req.params.id));
 if(!course) res.status(404).send('The course with the given ID does not exist');// 404
 res.send(course);
 });
+
 
 //PORT 
 const port = process.env.PORT || 3000;
